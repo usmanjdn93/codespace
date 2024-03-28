08249fffac2c2fd4c18237679bd2655eae1cbcc0
usman <usmanjadoon93@gmail.com>
2024-03-28 15:59:29 +0000

    homepage path update

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Usman Jadoon",
+  title: "Software Engineer & Data Scientist",
+  email: "usmanjadoon93@gmail.com",
+  gitHub: "usmanjdn93",
+  instagram: "usmanjdn",
+  linkedIn: "usman-jadoon-0b0423133",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
